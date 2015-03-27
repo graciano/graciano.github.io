b0285c18e26f1dfe7ba68fe5c9d1cd4e7b89221d
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2015-03-27 19:06:44 -0300

    efeito de toque adicionado

--- js/efeito-legal.js
+++ js/efeito-legal.js
@@ -3,8 +3,9 @@
  */
 (function(){
 
+
     var TEMPO_MINIMO = 20;
-    var VARIACAO_DELAY = 200;
+    var VARIACAO_DELAY = 60;
     var INTERVALO_TROCA = 15;
     var CARACTERES = ['1', '0', '!', '@', '#', '$', '%', '*', '(', ')', '£', '¢', '¬', '{', '[', ']', '}', '^', '<', '>', ',', '.', ';', ':', '?', '/', '|', '\\', "'", '"', '-', '_', '=', '+', '§'];
     var timeoutsEfeito = [];
@@ -26,7 +27,6 @@
         timeoutsEfeito = [];
     };
 
-
     var tempo = function(){
         return TEMPO_MINIMO + parseInt(Math.random() * VARIACAO_DELAY);
     };
@@ -46,37 +46,54 @@
         timeoutsEfeito.push(setTimeout(function(){ callback(arg0, arg1); }, tam*TEMPO_MINIMO + VARIACAO_DELAY));
     };
 
-   var projetos = document.getElementsByClassName('js-projeto');
-   for(var i=0; i<projetos.length; i++){
-       var projeto = projetos[i];
-       projeto.setAttribute('data-state', 'false');
+    var disparaEvento = function(ev, context){
+        var caption = context.getElementsByClassName('caption')[0];
+        var text = caption.textContent;
+        limpaTimeouts();
+        zeraEPreenche(caption, function(c, text){
+            var tam = text.length;
+            var t = TEMPO_MINIMO;
+            for(var i=0; i<tam; i++){
+                timeoutsEfeito.push(setTimeout(
+                    function(i){
+                        trocaChar(caption, text.substring(i, i+1), i);
+                    }, t + tempo(), i));
+                t+=TEMPO_MINIMO;
+            }
+        }, caption, text);
+        var event = ev || window.event;
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    };
 
-       projeto.addEventListener('mouseenter', function(ev){
-           var caption = this.getElementsByClassName('caption')[0];
-           var text = caption.textContent;
-           limpaTimeouts();
-           zeraEPreenche(caption, function(c, text){
-               var tam = text.length;
-               var t = TEMPO_MINIMO;
-               for(var i=0; i<tam; i++){
-                   timeoutsEfeito.push(setTimeout(
-                       function(i){
-                           trocaChar(caption, text.substring(i, i+1), i);
-                       }, t + tempo(), i));
-                   t+=TEMPO_MINIMO;
-               }
-           }, caption, text);
-           var event = ev || window.event;
-           if (event.stopPropagation) {
-               event.stopPropagation();
-           } else {
-               event.cancelBubble = true;
-           }
-       });
-       projeto.addEventListener('mouseleave', function(){
-           limpaTimeouts();
-           var caption = this.getElementsByClassName('caption')[0];
-           caption.textContent = caption.getAttribute('data-text');
-       });
-   }
+    var desligaEvento = function(ev, context){
+        limpaTimeouts();
+        var caption = context.getElementsByClassName('caption')[0];
+        caption.textContent = caption.getAttribute('data-text');
+    };
+
+    var projetos = document.getElementsByClassName('js-projeto');
+    for(var i=0; i<projetos.length; i++){
+
+        var projeto = projetos[i];
+        projeto.setAttribute('data-state', 'false');
+
+        projeto.addEventListener('mouseenter', function(ev){
+            disparaEvento(ev, this);
+            ev.preventDefault();
+        });
+        projeto.addEventListener('mouseleave', function(ev){
+            desligaEvento(ev, this);
+            ev.preventDefault();
+        });
+        projeto.addEventListener('touchstart', function(ev){
+            disparaEvento(ev, this);
+        });
+        projeto.addEventListener('touchend', function(ev){
+            desligaEvento(ev, this);
+        });
+    }
 })();
