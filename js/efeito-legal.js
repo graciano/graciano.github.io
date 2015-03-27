8b7619298d78fcd7b457a3368dbdf94c83a2e0dc
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2015-03-26 23:18:33 -0300

    problema de propagação do evento resolvido

--- js/efeito-legal.js
+++ js/efeito-legal.js
@@ -6,12 +6,7 @@
     var TEMPO_MINIMO = 200;
     var VARIACAO_DELAY = 500;
     var INTERVALO_TROCA = 15;
-    var DURACAO_EFEITO = 2* (TEMPO_MINIMO + VARIACAO_DELAY);
-    var CARACTERES = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '*', '(', ')', '£', '¢', '¬', '{', '[', ']', '}', '^', '<', '>', ',', '.', ';', ':', '?', '/', '|', '\\', "'", '"', '-', '_', '=', '+', '§'];
-
-    var geraTempoAleatorio = function(){
-        return parseInt(Math.random() * (DURACAO_EFEITO - TEMPO_MINIMO))+TEMPO_MINIMO;
-    };
+    var CARACTERES = ['1', '0', '!', '@', '#', '$', '%', '*', '(', ')', '£', '¢', '¬', '{', '[', ']', '}', '^', '<', '>', ',', '.', ';', ':', '?', '/', '|', '\\', "'", '"', '-', '_', '=', '+', '§'];
 
     var charAleatorio = function(){
         return CARACTERES[parseInt(Math.random() * (CARACTERES.length-1))];
@@ -40,36 +35,31 @@
         setTimeout(function(){ callback(arg0, arg1); }, TEMPO_MINIMO + VARIACAO_DELAY);
     };
 
-    var mostraLetras = function(elem){
-        var caption = elem.getElementsByClassName('caption')[0];
-        var text = caption.textContent;
-        zeraEPreenche(caption, function(c, text){
-            var tam = text.length;
-            for(var i=0; i<tam; i++){
-                var tempo = TEMPO_MINIMO + parseInt(Math.random() * VARIACAO_DELAY);
-                setTimeout(
-                    (function(i){
-                        caption.textContent = caption.textContent.substring(1, tam)+text[i];
-                    })(i), tempo);
-            }
-        }, caption, text);
-    };
-
    var projetos = document.getElementsByClassName('js-projeto');
    for(var i=0; i<projetos.length; i++){
        var projeto = projetos[i];
        projeto.setAttribute('data-state', 'false');
 
-       projeto.addEventListener('mouseover', function(){
-           if(this.getAttribute('data-state')=='true')
-               return false;
-           this.setAttribute('data-state', 'true');
-           setTimeout((function(elem){
-               elem.setAttribute('data-state', 'false');
-           })(this), DURACAO_EFEITO);
-           mostraLetras(this);
+       projeto.addEventListener('mouseenter', function(ev){
+           var caption = this.getElementsByClassName('caption')[0];
+           var text = caption.textContent;
+           zeraEPreenche(caption, function(c, text){
+               var tam = text.length;
+               for(var i=0; i<tam; i++){
+                   setTimeout(
+                       (function(i){
+                           caption.textContent = caption.textContent.substring(1, tam)+text[i];
+                       })(i), TEMPO_MINIMO + parseInt(Math.random() * VARIACAO_DELAY));
+               }
+           }, caption, text);
+           var event = ev || window.event;
+           if (event.stopPropagation) {
+               event.stopPropagation();
+           } else {
+               event.cancelBubble = true;
+           }
        });
-       projeto.addEventListener('mouseout', function(){
+       projeto.addEventListener('mouseleave', function(){
            var caption = this.getElementsByClassName('caption')[0];
            caption.textContent = caption.getAttribute('data-text');
        });
