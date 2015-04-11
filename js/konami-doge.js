42095e560841fcd55af07c90256b8c15ee086e9c
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2015-04-10 23:43:13 -0300

    animação do konami code com fade

--- js/konami-doge.js
+++ js/konami-doge.js
@@ -1,5 +1,6 @@
 (function(){
     var easter_egg = new Konami();
+    var NUMBER_OF_ANIMATIONS = 4;
     easter_egg.code = function() {
         console.log("konami code");
         var konamiDiv = document.getElementById('konami');
@@ -7,9 +8,18 @@
 
         xhr.onreadystatechange = function (e) {
             if (xhr.readyState == 4 && xhr.status == 200) {
-                konamiDiv.style.left = '0';
-                konamiDiv.className = 'slideLeft';
                 konamiDiv.innerHTML = xhr.responseText;
+                konamiDiv.style.display = '';
+                var children = konamiDiv.children;
+                for(var i=0; i<children.length; i++){
+                    var elem = children[i];
+                    elem.classList.add('animate-random-'+parseInt(Math.random() * NUMBER_OF_ANIMATIONS));
+                     //elem.style.webkitAnimationPlayState = 'running';
+                     //   elem.style.mozAnimationPlayState = 'running';
+                     //    elem.style.msAnimationPlayState = 'running';
+                     //     elem.style.oAnimationPlayState = 'running';
+                     //      elem.style.animationPlayState = 'running';
+                }
             }
         };
 
