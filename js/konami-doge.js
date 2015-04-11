59eb70ecf234ef01fcd370cafe48c1845771fc84
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2015-04-11 00:26:19 -0300

    doge smile

--- js/konami-doge.js
+++ js/konami-doge.js
@@ -1,8 +1,18 @@
 (function(){
     var easter_egg = new Konami();
     var NUMBER_OF_ANIMATIONS = 4;
-    easter_egg.code = function() {
-        console.log("konami code");
+    var loaded = false;
+
+    var preLoad = function(url, callback){
+        var image = new Image();
+        image.src = url;
+        image.onload = function(){
+            callback();
+        };
+    };
+
+    var doge = function(){
+        loaded = true;
         var konamiDiv = document.getElementById('konami');
         var xhr = new XMLHttpRequest();
 
@@ -14,18 +24,28 @@
                 for(var i=0; i<children.length; i++){
                     var elem = children[i];
                     elem.classList.add('animate-random-'+parseInt(Math.random() * NUMBER_OF_ANIMATIONS));
-                     //elem.style.webkitAnimationPlayState = 'running';
-                     //   elem.style.mozAnimationPlayState = 'running';
-                     //    elem.style.msAnimationPlayState = 'running';
-                     //     elem.style.oAnimationPlayState = 'running';
-                     //      elem.style.animationPlayState = 'running';
                 }
+                preLoad('imagens/doge-smile.png', function(){
+                    setTimeout(function(){
+                        var img = konamiDiv.getElementsByTagName('img')[0];
+                        img.src = 'imagens/doge-smile.png';
+                    }, 2000);
+                });
             }
         };
 
         xhr.open("GET", "doge.html", true);
         xhr.setRequestHeader('Content-type', 'text/html');
         xhr.send();
+    };
+
+
+    easter_egg.code = function() {
+        if(loaded)
+            doge();
+        else{
+            preLoad('imagens/doge.png', doge);
+        }
     };
     easter_egg.load();
 })();
