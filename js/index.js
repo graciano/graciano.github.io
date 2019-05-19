2d890811b8ff150ca48d065f0d7cc299112a4d97
Matheus Graciano (akira) <matheusgraciano@protonmail.com>
2019-05-19 12:59:01 -0300

    titulo do saite embaralha de 10 em 10 segundos

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-console.log('koé mundo!');
+import { embaralha } from 'embaralha';
+
+const tempoEmbaralha = 800;
+const intervaloEmbaralha = 10 * 1000;
+const h1 = document.querySelector('.h1-embaralha');
+
+setInterval(() => embaralha(h1, tempoEmbaralha), intervaloEmbaralha);
