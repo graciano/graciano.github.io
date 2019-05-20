86007ec82597f1b7715b8d3791304ac587402b8d
Matheus Graciano <mgraciano.terceiro@brasilcap.com.br>
2019-05-20 15:13:04 +0000

    5 segundos para o efeito

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import { embaralha } from 'embaralha';
 
 const tempoEmbaralha = 800;
-const intervaloEmbaralha = 10 * 1000;
+const intervaloEmbaralha = 5 * 1000;
 const h1 = document.querySelector('.h1-embaralha');
 
 setInterval(() => embaralha(h1, tempoEmbaralha), intervaloEmbaralha);
