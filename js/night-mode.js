6d767781600c7b7f462fac8f35dec5501302f26a
Matheus Graciano <mgraciano.terceiro@brasilcap.com.br>
2019-06-03 20:07:47 +0000

    tema default de acordo com o horário do cliente

--- js/night-mode.js
+++ js/night-mode.js
@@ -1,13 +1,18 @@
 const THEME_DEFAULT = 'theme--default';
 const THEME_DARK = 'theme--dark';
 const ACTIVE = 'active';
+const NIGHT_START = 19;
+const NIGHT_END = 6;
+const HOUR_NOW = (new Date()).getHours();
 
 const btnNightMode = document.querySelector('#btn-night-mode');
 const htmlElem = document.querySelector('html');
 
+const isNight = () => NIGHT_START <= HOUR_NOW || HOUR_NOW <= NIGHT_END;
+const themeByCurrentHour = () => isNight() ? THEME_DARK : THEME_DEFAULT;
 const nightModeActive = theme => theme === THEME_DARK;
 const otherTheme = theme => theme === THEME_DEFAULT ? THEME_DARK : THEME_DEFAULT;
-const currentTheme = () => localStorage.getItem('theme') || THEME_DEFAULT;
+const currentTheme = () => localStorage.getItem('theme') || themeByCurrentHour();
 const setInitialTheme = () => htmlElem.classList.add(currentTheme());
 const toggleHtmlThemeClass = () => {
   const theme = currentTheme();
